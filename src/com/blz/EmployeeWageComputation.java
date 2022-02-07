aa1be5a583f5b93767da60583bf383c4d692507d
susobhankr <susobhan3114@gmail.com>
2022-02-07 19:32:09 +0530

    Added:SwitchCase implementation

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -2,26 +2,33 @@ package com.blz;
 
 public class EmployeeWageComputation {
 
-	public static void main(String[] args) {
-		
-		//Constant
-				int IS_PRESENT = 1;
-				int IS_PART_TIME = 2;
-				int SALARY_PER_HOUR = 20;
-				//Variables
-				double empCheck = Math.floor(Math.random() * 10) % 3;
-				int empHr = 0;
-				if (empCheck == IS_PRESENT) {
-					empHr = 8;
-				} else if (empCheck == IS_PART_TIME) {
-					empHr = 4;
-				} else {
-					empHr = 0;
-				}
-				double empSalary = empHr * SALARY_PER_HOUR;
-				
-				System.out.println("Emp Wage : "+empSalary);
+		static final int IS_PRESENT = 1;
+		static final int IS_PART_TIME = 2;
+		static final int SALARY_PER_HOUR = 20;
 
+		public static void main(String[] args) {
+			//Variables
+			int empCheck;
+			empCheck = (int)(Math.floor(Math.random() * 10) % 3);
+			int empHr = 0;
+
+			switch(empCheck) {
+				case IS_PRESENT :
+					empHr = 8;
+				break;
+
+				case IS_PART_TIME :
+					empHr=4;
+				break;
+
+				default :
+					empHr=0;
+				break;
+			}
+
+			double empSalary = empHr * SALARY_PER_HOUR;
+			
+			System.out.println("Emp Wage : "+empSalary);
 	}
 
 }
