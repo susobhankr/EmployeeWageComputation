7060977603133e4d21f8355495866a5b2da9b72d
susobhankr <susobhan3114@gmail.com>
2022-02-07 20:03:46 +0530

    Added:Monthly salary of the employee

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -1,17 +1,17 @@
 package com.blz;
 
 public class EmployeeWageComputation {
-
-		static final int IS_PRESENT = 1;
-		static final int IS_PART_TIME = 2;
-		static final int SALARY_PER_HOUR = 20;
-
-		public static void main(String[] args) {
-			//Variables
-			int empCheck;
+	static final int IS_PRESENT = 1;
+	static final int IS_PART_TIME = 2;
+	static final int SALARY_PER_HOUR = 20;
+	static final int TOTAL_WORK_DAY = 20;
+	public static void main(String[] args) {
+		//Variables
+		int empCheck;
+		int empHr = 0;
+		int totalSalary=0;
+		for (int i=1;i<=20;i++) {
 			empCheck = (int)(Math.floor(Math.random() * 10) % 3);
-			int empHr = 0;
-
 			switch(empCheck) {
 				case IS_PRESENT :
 					empHr = 8;
@@ -25,10 +25,12 @@ public class EmployeeWageComputation {
 					empHr=0;
 				break;
 			}
-
 			double empSalary = empHr * SALARY_PER_HOUR;
-			
-			System.out.println("Emp Wage : "+empSalary);
+			//totalSalary+=empSalary;	
+			 totalSalary = totalSalary + (int)empSalary;
+		}
+		System.out.println("Monthly Emp Wage : "+totalSalary);
 	}
-
 }
+
+
