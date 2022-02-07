df6d00f42b6516da65ecd7795fcc9b408e6798fe
susobhankr <susobhan3114@gmail.com>
2022-02-07 10:37:33 +0530

    Added: Daily wage logic

--- src/com/blz/EmployeeWageComputation.java
+++ src/com/blz/EmployeeWageComputation.java
@@ -6,14 +6,16 @@ public class EmployeeWageComputation {
 		
 		//Constant
 				int IS_PRESENT = 1;
-				
+				int SALARY_PER_HOUR = 20;
+				//Variables
 				double empCheck = Math.floor(Math.random() * 10) % 2;
-
+				int workingHour = 0;
+				if (empCheck == IS_PRESENT) {
+					workingHour=8;
+				}
+				double empSalary = workingHour * SALARY_PER_HOUR;
 				
-				if (empCheck == IS_PRESENT)
-					System.out.println("Employee is present");
-				else
-					System.out.println("Employee is absent");
+				System.out.println("Employee has earned "+empSalary+" dollar today.");
 
 	}
 
